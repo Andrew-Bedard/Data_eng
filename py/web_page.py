5bebbe86d166c27510c0bb1e5d2ee2abe6c66bcd
Andrew-Bedard <andrewjeanbedard@gmail.com>
2021-08-01 23:55:12 +0200

    first prototype displaying multiple banners

--- py/web_page.py
+++ py/web_page.py
@@ -10,8 +10,10 @@ app.config['UPLOAD_FOLDER'] = BANNER_FOLDER
 @app.route("/")
 @app.route('/index')
 def show_index():
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'image_100.png')
-    return render_template("index.html", user_image=full_filename)
+    imageList = os.listdir('static/banner_images/images')
+    imageList = ['banner_images/images/' + image for image in imageList]
+    imageList = imageList[0:5]
+    return render_template("index.html", imageList=imageList)
 
 if __name__ == "__main__":
     app.run()
