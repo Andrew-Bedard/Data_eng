33a6bd5abdeecb34e5570d7437e395bb49bdfdaf
Andrew-Bedard <andrewjeanbedard@gmail.com>
2021-08-03 01:31:00 +0200

    first working prototype with dynamic url, fixed issue with campaign id sanitation

--- py/banner_logic.py
+++ py/banner_logic.py
@@ -6,21 +6,37 @@ Collection of functions for logic on which banners to display
 import pandas as pd
 from datetime import datetime as dt
 
-t_ind = 4
+def load_frames(dat_path):
 
-dat_path = "D:\\Projects\\Data_eng\\py\\data\\csv\\csv\\"
+    """
 
-clicks_df = pd.read_csv(dat_path + f"{t_ind}\\clicks_{t_ind}.csv")
-conv_df = pd.read_csv(dat_path + f"{t_ind}\\conversions_{t_ind}.csv")
-imp_df = pd.read_csv(dat_path + f"{t_ind}\\impressions_{t_ind}.csv")
+    :param dat_path:
+    :return:
+    """
 
-# X is the number of banners with conversions within a campaign
-# X>10
+    current_minute = dt.now().minute
 
-conv_click_df = pd.merge(conv_df, clicks_df, how='outer', on='click_id')
+    if current_minute in range(16):
+        t_ind = 1
+    elif current_minute in range(16, 31):
+        t_ind = 2
+    elif current_minute in range(31, 46):
+        t_ind = 3
+    else:
+        t_ind = 4
+
+    clicks = pd.read_csv(dat_path + f"{t_ind}\\clicks_{t_ind}.csv")
+    conv = pd.read_csv(dat_path + f"{t_ind}\\conversions_{t_ind}.csv")
+    imp = pd.read_csv(dat_path + f"{t_ind}\\impressions_{t_ind}.csv")
+
+    return clicks, conv, imp
 
 
 def get_banners(imp_df, conv_click_df, camp_id):
+
+    if not type(camp_id) == int:
+        camp_id = int(camp_id)
+
     camp_frame = conv_click_df[conv_click_df['campaign_id'] == camp_id]
 
     if len(camp_frame[~camp_frame['conversion_id'].isna()]['banner_id'].unique()) >= 10:
@@ -58,3 +74,13 @@ def get_banners(imp_df, conv_click_df, camp_id):
             dummy_list.append(list(dummy_series.sample(5-len(dummy_list)).values))
 
         return list(pd.core.common.flatten(dummy_list))
+
+
+# dat_path = "D:\\Projects\\Data_eng\\py\\data\\csv\\csv\\"
+#
+# clicks_df, conv_df, imp_df = load_frames(dat_path)
+#
+# conv_click_df = pd.merge(conv_df, clicks_df, how='outer', on='click_id')
+#
+# thing = get_banners(imp_df, conv_click_df, camp_id)
+
--- py/web_page.py
+++ py/web_page.py
@@ -5,19 +5,28 @@ Flask webapp to display banners
 
 from flask import Flask, render_template
 import os
+from banner_logic import *
 
 BANNER_FOLDER = os.path.join('static', 'banner_images', 'images')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = BANNER_FOLDER
 
+# thing = [494, 439, 190, 261, 116, 275, 452]
+# thing = [str(i) for i in thing]
+
+clicks_df, conv_df, imp_df = load_frames("D:\\Projects\\Data_eng\\py\\data\\csv\\csv\\")
+conv_click_df = pd.merge(conv_df, clicks_df, how='outer', on='click_id')
+
 # Landing page
-@app.route("/")
-@app.route('/index')
-def show_index():
+#@app.route("/")
+@app.route("/campaigns/<campaign_id>")
+def show_index(campaign_id):
+    banner_list = get_banners(imp_df, conv_click_df, campaign_id)
+    banner_list = [str(i) for i in banner_list]
     imageList = os.listdir('static/banner_images/images')
     imageList = ['banner_images/images/' + image for image in imageList]
-    imageList = imageList[0:5]
+    imageList = [image for image in imageList if image[-7:-4] in banner_list]
     return render_template("index.html", imageList=imageList)
 
 if __name__ == "__main__":
