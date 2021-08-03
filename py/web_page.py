005246f8bf6bcec11524d63e40505dfc0b9aa3d4
Andrew-Bedard <andrewjeanbedard@gmail.com>
2021-08-03 13:39:28 +0200

    added banner order randomization

--- py/web_page.py
+++ py/web_page.py
@@ -3,16 +3,17 @@ Flask webapp to display banners
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import os
 from banner_logic import *
+import random
 
 BANNER_FOLDER = os.path.join('static', 'banner_images', 'images')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = BANNER_FOLDER
 
-# thing = [494, 439, 190, 261, 116, 275, 452]
+thing = [494, 439, 190, 261, 116, 275, 452]
 # thing = [str(i) for i in thing]
 
 clicks_df, conv_df, imp_df = load_frames("D:\\Projects\\Data_eng\\py\\data\\csv\\csv\\")
@@ -27,7 +28,12 @@ def show_index(campaign_id):
     imageList = os.listdir('static/banner_images/images')
     imageList = ['banner_images/images/' + image for image in imageList]
     imageList = [image for image in imageList if image[-7:-4] in banner_list]
+    random.shuffle(imageList)
     return render_template("index.html", imageList=imageList)
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
 
 if __name__ == "__main__":
     app.run()
